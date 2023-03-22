30e168a2432848f443bc9d432b39fb4591864daa
Ruslan Formanchuk <ruslan12032@gmail.com>
2023-03-22 13:26:11 +0200

    start

---
+++ index.js
@@ -0,0 +1,80 @@
+let a = "";
+let b = "";
+let sign = "";
+let finish = false;
+
+const digit = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."];
+const action = ["-", "+", "X", "/"];
+
+const out = document.querySelector(".calc-screen p");
+
+function clearAll() {
+  a = "";
+  b = "";
+  sign = "";
+  finish = false;
+  out.textContent = 0;
+}
+
+document.querySelector(".ac").onclick = clearAll;
+
+document.querySelector(".buttons").onclick = (event) => {
+  //нажата не кнопка
+  if (!event.target.classList.contains("btn")) return;
+  // нажата кнопка ac
+  if (event.target.classList.contains("ac")) return;
+
+  out.textContent = "";
+  //отримую нажату кнопку
+  const key = event.target.textContent;
+
+  // якщо нажата кнопка 0-9
+  if (digit.includes(key)) {
+    if (b === "" && sign === "") {
+      a += key;
+      out.textContent = a;
+    } else if (a !== "" && b !== "" && finish) {
+      b = key;
+      finish = false;
+      out.textContent = b;
+    } else {
+      b += key;
+      out.textContent = b;
+    }
+    return;
+  }
+
+  // якщо нажата кнопка + - / *
+  if (action.includes(key)) {
+    sign = key;
+    out.textContent = sign;
+    return;
+  }
+  // якщо нажата кнопка =
+  if (key === "=") {
+    if (b === "") b = a;
+    switch (sign) {
+      case "+":
+        a = +a + +b;
+        break;
+      case "-":
+        a = a - b;
+        break;
+      case "X":
+        a = a * b;
+        break;
+      case "/":
+        if (b === "0") {
+          out.textContent = "Помилка";
+          a = "";
+          b = "";
+          sign = "";
+          return;
+        }
+        a = a / b;
+        break;
+    }
+    finish = true;
+    out.textContent = a;
+  }
+};
